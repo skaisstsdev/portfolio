b4db9aba47b359357e0401c874484af502604fb2
«skai»sstsdev <skaissts.dev@gmail.com>
2026-06-21 20:33:28 +0200

    Revert hero tilt timing to original linear mapping

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -31,9 +31,8 @@ export const ContainerScroll = ({
     return [1.05, 1];
   };
 
-  const scaleVals = scaleDimensions();
-  const rotate = useTransform(scrollYProgress, [0, 0.5, 1], [isMobile ? 40 : 30, isMobile ? 40 : 30, 0]);
-  const scale = useTransform(scrollYProgress, [0, 0.5, 1], [scaleVals[0], scaleVals[0], scaleVals[1]]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [isMobile ? 40 : 30, 0]);
+  const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions());
 
   return (
     <div
